a9a6079c378be2dc9420b9f91bd2e84c98fb5bfe
phaeryl <florian.arnaud2019@gmail.com>
2020-01-13 16:32:17 +0100

    Exercise 5 finished.

---
+++ exercice-4/script.ts
@@ -0,0 +1,24 @@
+let arrayString: string[] = ["foo", "bar", "baz", "qux", "quux", "corge", "grault"];
+function isSingle (array: string[], stringIsSingle: string): boolean {
+    let i: number=0;
+    let j: number=0;
+    let isSingle: boolean;
+    let currentElement: string;
+    while (i < array.length) {
+        currentElement = array[i];
+        
+        if (stringIsSingle == currentElement) {
+            j++;
+        }
+        i++;
+    }
+    if(j > 1) {
+        isSingle = false;
+    }
+    // isSingle = true, even if the string doesn't appear in array.
+    else {
+        isSingle = true;
+    }
+    return isSingle;
+}
+console.log(isSingle(arrayString, "bar"));
