f11639cce9791270772571a11f2d3c028ff2ce4f
phaeryl <florian.arnaud2019@gmail.com>
2020-01-13 16:57:36 +0100

    Exercise 5 started.

---
+++ exercice-5/script.py
@@ -0,0 +1,9 @@
+stringArray = []"foo", "bar", "bar", "baz", "qux", "quux", "corge", "grault"]
+def checkUniqueness(array):
+    i=0
+    currentElement= ""
+    while i <len(array):
+        currentElement = array[i]
+        i = i + 1
+        
+
