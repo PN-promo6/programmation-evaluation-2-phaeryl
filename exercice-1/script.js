a7c2d23b92268c25c0409e4c78786a76ba807e23
phaeryl <florian.arnaud2019@gmail.com>
2020-01-13 14:50:49 +0100

    First exercise indent fixed.

--- exercice-1/script.js
+++ exercice-1/script.js
@@ -10,7 +10,7 @@ function displayArray(array) {
     let headerData = array[0];
     let table = document.createElement("table");
     document.body.appendChild(table);
-    table.style.border="1px solid black";
+    table.style.border = "1px solid black";
     let tableHeader = document.createElement("thead");
     table.appendChild(tableHeader);
     let tableBody = document.createElement("tbody");
@@ -18,36 +18,34 @@ function displayArray(array) {
     let tableHeaderRow = document.createElement("tr");
     tableHeader.appendChild(tableHeaderRow);
     let i = 0;
-    let j=1;
-    
-    while (i < headerData.length) {
-        let currentElement=headerData[i];
-        let tableHeaderCell= document.createElement("th");
-        tableHeaderRow.appendChild(tableHeaderCell);
-        tableHeaderCell.textContent=currentElement;
-        tableHeaderCell.style.color="red";
-        i++;
-        }
+    let j = 1;
 
-    while (j <array.length){
-        let currentElement2=array[j];
+    while (i < headerData.length) {
+        let currentElement = headerData[i];
+        let tableHeaderCell = document.createElement("th");
+        tableHeaderRow.appendChild(tableHeaderCell);
+        tableHeaderCell.textContent = currentElement;
+        tableHeaderCell.style.color = "red";
+        i++;
+    }
+
+    while (j < array.length) {
+        let currentElement2 = array[j];
         let tableBodyRow = document.createElement("tr");
         tableBody.appendChild(tableBodyRow);
-        j++; 
-        let k=0;
+        j++;
+        let k = 0;
 
         while (k < currentElement2.length) {
             let tableBodyCell = document.createElement("td");
             tableBodyRow.appendChild(tableBodyCell);
-            tableBodyCell.textContent=currentElement2[k];
-            if (currentElement2[k] == currentElement2[3])
-            {
-                tableBodyCell.style.fontStyle="italic";
+            tableBodyCell.textContent = currentElement2[k];
+            if (currentElement2[k] == currentElement2[3]) {
+                tableBodyCell.style.fontStyle = "italic";
             }
-            k++;            
+            k++;
         }
 
     }
 }
 displayArray(fishList);
-
